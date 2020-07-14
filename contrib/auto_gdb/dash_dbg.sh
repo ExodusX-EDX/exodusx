6b7df7ee8894af8787ee9f9adddcc52028bc3802
CowsGoWuff <20851921+CowsGoWuff@users.noreply.github.com>
2020-07-13 20:28:16 -0700

    Rebrand

--- contrib/auto_gdb/dash_dbg.sh
+++ contrib/auto_gdb/dash_dbg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# use testnet settings,  if you need mainnet,  use ~/.dashcore/dashd.pid file instead
-dash_pid=$(<~/.dashcore/testnet3/dashd.pid)
-sudo gdb -batch -ex "source debug.gdb" dashd ${dash_pid}
+# use testnet settings,  if you need mainnet,  use ~/.exodusxcore/exodusxd.pid file instead
+exodusx_pid=$(<~/.exodusxcore/testnet3/exodusxd.pid)
+sudo gdb -batch -ex "source debug.gdb" exodusxd ${exodusx_pid}
